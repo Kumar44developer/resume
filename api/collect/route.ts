f8b5176fcd5551128d17baaae5a5a3e38ec36847
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2026-08-10 21:26:14 +0530

    Added imports for Product Hunt and Stack Overflow fetchers

--- api/collect/route.ts
+++ api/collect/route.ts
@@ -4,3 +4,5 @@ import type { FetchResult, RawPost } from "@/lib/fetchers/types";
 import { fetchRedditPosts } from "@/lib/fetchers/reddit";
 import { fetchHackerNewsPosts } from "@/lib/fetchers/hackernews";
 import { fetchGithubIssues } from "@/lib/fetchers/github";
+import { fetchProductHuntComments } from "@/lib/fetchers/producthunt";
+import { fetchStackOverflowQuestions } from "@/lib/fetchers/stackoverflow";
